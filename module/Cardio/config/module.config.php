4426bb35606154d4b18280f12c7f7b023f232be2
Dunca Denisa <denisa.dunca@qinsoft.ro>
2022-11-02 14:17:24 +0200

    commit

--- module/Cardio/config/module.config.php
+++ module/Cardio/config/module.config.php
@@ -3,95 +3,19 @@
 namespace Cardio;
 
 
-use Laminas\Router\Http\Literal;
-use Laminas\Router\Http\Segment;
-
 return[
     'controllers' => [
         'factories' => [
-            Controller\LoginController::class => Controller\Factory\LoginControllerFactory::class,
-            Controller\AuthController::class => Controller\Factory\AuthControllerFactory::class,
-            Controller\PatientController::class => Controller\Factory\PatientControllerFactory::class,
-        ],
+           ],
     ],
 
     'router' => [
         'routes' => [
-            'login' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/login[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\LoginController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-            'signup' => [
-                'type'    => Literal::class,
-                'options' => [
-                    'route' => '/signup',
-                    'defaults' => [
-                        'controller' => Controller\UserController::class,
-                        'action'     => 'add',
-                    ],
-                ],
-            ],
-            'welcome' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/welcome[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\UserController::class,
-                        'action'     => 'welcome',
-                    ],
-                ],
-            ],
-            'user' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/user[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\UserController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-            'patient' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/patient[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\PatientController::class,
-                        'action'     => 'patient',
-                    ],
-                ],
-            ],
+
         ],
     ],
     'view_manager' => [
         'template_path_stack' => [
-            'user' => __DIR__ . '/../view',
-            'login'=>__DIR__ . '/../view',
-            'signup'=>__DIR__ . '/../view',
-            'welcome'=>__DIR__ . '/../view',
-            'patient'=>__DIR__ . '/../view',
         ],
     ],
 ];
--- module/Cardio/src/Module.php
+++ module/Cardio/src/Module.php
@@ -13,36 +13,4 @@ class Module implements ConfigProviderInterface
     {
         return include __DIR__ . '/../config/module.config.php';
     }
-
-    public function getServiceConfig()
-    {
-        return [
-            'factories' => [
-                Model\UserTable::class => function($container) {
-                    $tableGateway = $container->get(Model\UserTableGateway::class);
-                    return new Model\UserTable($tableGateway);
-                },
-                Model\UserTableGateway::class => function ($container) {
-                    $dbAdapter = $container->get(AdapterInterface::class);
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Model\User());
-                    return new TableGateway('Users', $dbAdapter, null, $resultSetPrototype);
-                },
-            ],
-        ];
-    }
-    public function getControllerConfig()
-    {
-        return [
-            'factories' => [
-                Controller\UserController::class => function($container) {
-                    return new Controller\UserController(
-                        $container->get(Model\UserTable::class)
-                    );
-                },
-            ],
-        ];
-    }
-
-
 }

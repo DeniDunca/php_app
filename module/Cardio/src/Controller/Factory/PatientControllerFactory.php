fd7dda2ecfa47b0fa97ab11c25ffbc050d2e45f6
Dunca Denisa <denisa.dunca@qinsoft.ro>
2022-11-02 13:53:49 +0200

    commit

--- module/Cardio/src/Controller/Factory/PatientControllerFactory.php
+++ module/Cardio/src/Controller/Factory/PatientControllerFactory.php
@@ -9,6 +9,7 @@ use Psr\Container\ContainerInterface;
 
 class PatientControllerFactory implements FactoryInterface
 {
+
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
         return new PatientController(
